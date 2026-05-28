b966d0cdae48066b770dc1cc23567959a01ad013
RachelWang <niuniu0214wang@outlook.com>
2026-05-28 12:52:46 +0800

    Match profile list icon sizing

--- SmartBedControl/Views/MineCell.m
+++ SmartBedControl/Views/MineCell.m
@@ -7,6 +7,10 @@
 
 #import "MineCell.h"
 
+@interface MineCell ()
+@property (nonatomic, strong) UIView *iconContainer;
+@end
+
 @implementation MineCell
 
 - (void)awakeFromNib {
@@ -15,13 +19,19 @@
     self.detailLabel.textColor = [UIColor colorWithValue:@"#6b7280"];
     self.icon.contentMode = UIViewContentModeScaleAspectFit;
     self.icon.tintColor = [UIColor colorWithValue:@"#9ca3af"];
+    self.iconContainer = [[UIView alloc] initWithFrame:CGRectZero];
+    self.iconContainer.backgroundColor = [UIColor colorWithValue:@"#1a1a1a"];
+    self.iconContainer.layer.cornerRadius = 10.0;
+    self.iconContainer.layer.masksToBounds = YES;
+    [self.contentView insertSubview:self.iconContainer belowSubview:self.icon];
 }
 
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    self.icon.frame = CGRectMake(22, 19, 28, 28);
-    CGFloat textX = CGRectGetMaxX(self.icon.frame) + 16;
+    self.iconContainer.frame = CGRectMake(20, 13, 40, 40);
+    self.icon.frame = CGRectMake(30, 23, 20, 20);
+    CGFloat textX = 70;
     CGFloat textW = CGRectGetWidth(self.contentView.bounds) - textX - 18;
     self.titleLabel.frame = CGRectMake(textX, 13, textW, 20);
     self.detailLabel.frame = CGRectMake(textX, 34, textW, 20);
